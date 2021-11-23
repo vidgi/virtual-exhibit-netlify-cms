632b3cc552bce9dc633169b688dc61fe1acd106d
vidgi <vzg805@gmail.com>
2021-11-22 22:05:13 -0600

    trial with astronaut

--- src/templates/speciation-page.js
+++ src/templates/speciation-page.js
@@ -27,9 +27,8 @@ export const SpeciationPageTemplate = ({ title, content, contentComponent }) => 
       <div id="card">
       {(typeof window !== 'undefined') ? (
           <model-viewer
-          src="/Model_01.gltf"
+          src="/Astronaut.glb"
           ios-src=""
-          poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
           alt="A 3D model of an astronaut"
           shadow-intensity="1"
           camera-controls
